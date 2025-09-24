c91bfe1e2aceacf045a3c0f8f4cc25245711604a
dohylee03 <dohylee03@cu.ac.kr>
2025-09-24 04:32:34 +0000

    done

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include "copy.h"
 
+#define MAX_STRINGS 5
 char line[MAXLINE];
 char longest[MAXLINE];
 
-main(){
-	int len;
-	int max;
-	max = 0;
+int main(){
+	char strings[MAX_STRINGS][MAXLINE];
 
-	while(gets(line)!=NULL){
-		len = strlen(line);
-
-		if(len > max){
-			max = len;
-			copy(line, longest);
-		}
+	for(int i=0;i<MAX_STRINGS; i++){
+		fgets(strings[i], MAXLINE, stdin);
+		strings[i][strcspn(strings[i], "\n")] = 0;
 	}
 
-	if(max>0)
-		printf("%s \n", longest);
+	sort_strings(strings, MAX_STRINGS);
 
+	for (int i = 0; i < MAX_STRINGS; i++){
+		printf("%s\n", strings[i]);
+	}
 	return 0;
 }
 
+void sort_strings(char strings[MAX_STRINGS][MAXLINE], int count){
+	char temp[MAXLINE];
+	for (int i=0;i<count - 1; i++){
+		for(int j=0; j<count-i-1;j++){
+			if(strlen(strings[j]) < strlen(strings[j+1])) {
+				copy(strings[j], temp);
+				copy(strings[j+1], strings[j]);
+				copy(temp, strings[j+1]);
+				
+			}	
+		}	
+	}
+}
