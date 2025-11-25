a702bb9bef536f0284c1f046b266e646cb4ea338
dohylee03 <dohylee03@cu.ac.kr>
2025-11-25 01:52:23 +0000

    dulham

--- chap10/hw1/pp.c
+++ chap10/hw1/pp.c
@@ -13,11 +13,58 @@ void printStack(struct node *top);
 int main() {
 	struct node *head = (struct node*)malloc(sizeof(struct node));
 	head->next = NULL;
-	
+
 	int input;
+	int result;
 
-	while (1) {
-		scanf("%d", &input);
+	while (1){
+		result = scanf("%d", &input);
 
-		if (input <= 0){
-			printf("\n[
+		if (result != 1 || input <= 0){
+			while(getchar() != '\n');
+
+			printf("print stack\n");
+			printStack(head);
+			break;
+		}
+		else {
+			push(head, input);
+		}
+	}
+	return 0;
+}
+
+void push(struct node *top, int data){
+	struct node *newNode = (struct node*)malloc(sizeof(struct node));
+	newNode->data = data;
+	newNode->next = top->next;
+	top->next = newNode;
+}
+
+int pop(struct node *top){
+	if (top->next == NULL){
+		printf("Stack is Empty\n");
+		return -1;
+	}
+
+	struct node *delNode = top->next;
+	int returnData = delNode->data;
+
+	top->next = delNode->next;
+
+	free(delNode);
+	return returnData;
+}
+void printStack(struct node *top){
+	struct node *curr = top -> next;
+	if(curr == NULL){
+		printf("Stack is Empty\n");
+		return;
+	}
+
+	while (curr != NULL){
+		printf("%d \n", curr->data);
+		curr = curr->next;
+	}
+}
+
