f62d80cf985415617acf34e9a06d43421b20815b
dohylee03 <dohylee03@cu.ac.kr>
2025-12-03 00:19:32 +0000

    done

--- chap11/hw1/almhandler.c
+++ chap11/hw1/almhandler.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <signal.h>
+typedef void (*sighandler_t)(int);
+
+sighandler_t my_signal(int signo, sighandler_t handler){
+	struct sigaction act, old_act;
+	act.sa_handler = handler;
+
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+
+	if (sigaction(signo, &act, &old_act) < 0) {
+		return SIG_ERR;
+	}
+
+	return old_act.sa_handler;
+}
+
 void alarmHandler();
 int main(){
-	signal(SIGALRM, alarmHandler);
+	if(my_signal(SIGALRM, alarmHandler) == SIG_ERR){
+		perror("Signal error");
+		return 1;
+	}
+
 	alarm(5);
 	short i = 0;
 	while (1){
@@ -11,6 +31,7 @@ int main(){
 		printf("%d second\n", i);
 	}
 	printf("end\n");
+	return 0;
 }
 
 void alarmHandler(int signo)
